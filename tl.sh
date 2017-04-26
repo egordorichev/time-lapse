23b93f4d7ae0eaf1b59cbd3dea73f80518e4c2e8
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-04-26 14:51:29 +0300

    Create tl.sh

---
+++ tl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm img
+mkdir img
+clear
+echo "Making a screenshoot every 10 seconds..."
+
+while [ 1 ]; do
+	import -window root ./img/$(date +%y%m%d-%H%M%S).jpg
+	sleep 10
+done
+
+# make video with 
+# mencoder mf://img/*.jpg -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o output.avi
